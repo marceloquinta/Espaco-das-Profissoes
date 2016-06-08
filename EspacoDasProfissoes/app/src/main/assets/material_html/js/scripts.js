7afb3c042b5da954a4d6c43afd7b89e28193e2cb
anaflaviacador <aflaviah15@gmail.com>
2016-06-08 15:40:16 -0300

    Layout pag de curso e pag de programacao

--- EspacoDasProfissoes/app/src/main/assets/material_html/js/scripts.js
+++ EspacoDasProfissoes/app/src/main/assets/material_html/js/scripts.js
@@ -1,14 +1,29 @@
 $(document).ready(function() {
-  $('.button-collapse').sideNav({
-      edge: 'right',
-      closeOnClick: true
-    }
-  );
+    $('.ripple').on('click', function (event) {
+      event.preventDefault();
+      
+      var $div = $('<div/>'),
+          btnOffset = $(this).offset(),
+          xPos = event.pageX - btnOffset.left,
+          yPos = event.pageY - btnOffset.top;
+      
 
-  $('.sliderabas').bxSlider({
-    pagerCustom: '.tabs',
-    touchEnabled: false,
-    controls: false,
-    adaptiveHeight: true
-  });  
+      
+      $div.addClass('ripple-effect');
+      var $ripple = $(".ripple-effect");
+      
+      $ripple.css("height", $(this).height());
+      $ripple.css("width", $(this).height());
+      $div
+        .css({
+          top: yPos - ($ripple.height()/2),
+          left: xPos - ($ripple.width()/2),
+          background: $(this).data("ripple-color")
+        }) 
+        .appendTo($(this));
+
+      window.setTimeout(function(){
+        $div.remove();
+      }, 1400);
+    });
 });
