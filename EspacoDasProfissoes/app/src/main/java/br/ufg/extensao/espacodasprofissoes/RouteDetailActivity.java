14e3f459e6a6f4d6ee7fc54d015103750825bb14
krllus <joaocarlusferrera@gmail.com>
2017-06-13 13:29:42 -0300

    -> Manifest  - google maps test key
    -> Route Detail Activity
     - reworked how to display the route name

--- EspacoDasProfissoes/app/src/main/java/br/ufg/extensao/espacodasprofissoes/RouteDetailActivity.java
+++ EspacoDasProfissoes/app/src/main/java/br/ufg/extensao/espacodasprofissoes/RouteDetailActivity.java
@@ -20,8 +20,6 @@ public class RouteDetailActivity extends AppCompatActivity {
 
     private Route route;
 
-    private WebView webview;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +33,13 @@ public class RouteDetailActivity extends AppCompatActivity {
     private void setupToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Rota " + route.getName());
+        getSupportActionBar().setTitle(route.getName());
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
-    public void setupFragment(){
+    public void setupFragment() {
         WebFragment rotasFragment = new WebFragment();
         rotasFragment.setPageContent(PageContent.ROTAS_CARD);
         RouteWebInterface interfaceWeb = new RouteWebInterface(this);
